c5043ec533d39b9658809d5a88985ab55a9c7175
Luuk <luuk_wolferen@hotmail.com>
2019-11-15 13:06:53 +0100

    student toegevoegd

---
+++ src/student/Student.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const student = (props)=>{
+    return <div>
+        <h1>Ik ben een student</h1>
+        <h2>Ik ben 99 jaar oud</h2>
+        <h3>En ik hou van kaas</h3>
+    </div>
+}
+
+export default student;
