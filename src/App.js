d19efa3f3e5c805cf3bf630d10f1e7321e0004f8
Luuk <luuk_wolferen@hotmail.com>
2019-11-15 13:06:17 +0100

    student toegevoegd

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+  <h2>Daar gaan we {props.name}</h2>
+  <h3>Mijn favoriete kaassoort is {props.kaas}</h3>
+        
+        
       </header>
     </div>
   );
